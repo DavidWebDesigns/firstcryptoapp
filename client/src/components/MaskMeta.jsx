fe66a56d8ff9f1c29d2c978a6d4444f5691070f7
David Perez <javadavidperez@gmail.com>
2022-06-10 00:05:12 -0500

    Iframe added

--- client/src/components/MaskMeta.jsx
+++ client/src/components/MaskMeta.jsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline';
 
 const MaskMeta = () => {
     return (
-        <div className='-inset-y-10  absolute h-screen w-full z-0'>
-            <Spline scene="https://prod.spline.design/N2Bn0pgbgardmPwa/scene.splinecode" />
+        <div className='-inset-y-10 absolute mb-48 h-screen w-full z-0 overflow-hidden'>
+            <iframe src='https://my.spline.design/mymaskcopy-775ec23a01a5ace791cd044020af92d3/' frameborder='0' width='110%' height='100%'></iframe>
         </div>
     );
             }
